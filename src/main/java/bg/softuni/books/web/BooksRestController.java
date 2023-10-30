9865666b0ec5b876191d582370320b01a96e62c9
YanaMarkova <yana_markova@yahoo.com>
2023-10-30 16:02:31 +0100

    Initial commit

--- src/main/java/bg/softuni/books/web/BooksRestController.java
+++ src/main/java/bg/softuni/books/web/BooksRestController.java
@@ -4,10 +4,12 @@ import bg.softuni.books.model.dto.BookDTO;
 import bg.softuni.books.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -23,5 +25,14 @@ public class BooksRestController {
     public ResponseEntity<List<BookDTO>> getAllBooks() {
 
         return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> findBookById(@PathVariable("id") Long id) {
+
+        Optional<BookDTO> bookDTOOptional = bookService.findBookById(id);
+        return bookDTOOptional
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
